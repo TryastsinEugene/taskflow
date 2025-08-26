45842cbfff91df6b10c4ce7bdd3607f78afd8cc4
TryastsinEugene <eugenetryas@gmail.com>
2025-08-26 15:34:58 +0300

    adding suspense

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -4,15 +4,17 @@ import { CreateTaskModal } from "@/components/create-task-modal";
 import { EditTaskModal } from "@/components/edit-task-modal";
 import { TaskViewSwitcher } from "@/components/task-view-switcher";
 import { NuqsAdapter  } from "nuqs/adapters/next/app";
-
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <NuqsAdapter>
-        <TaskViewSwitcher />    
-        <CreateTaskModal /> 
-        <EditTaskModal />
+        <Suspense fallback="Loading..."></Suspense>
+          <TaskViewSwitcher />    
+          <CreateTaskModal /> 
+          <EditTaskModal />
+        </Suspense>
       </NuqsAdapter>
     </div>
      
